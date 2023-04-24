50af9cb520f4e39ce196881688734cdbf50dc27b
Nikhil Kumar <131350052+nikhilkr67@users.noreply.github.com>
2023-04-24 14:52:43 +0530

    Add files via upload

--- services/Impl/ReservationServiceImpl.java
+++ services/Impl/ReservationServiceImpl.java
@@ -1,1 +1,56 @@
+package com.nikhil.services;
+
+
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nikhil.dto.ReservationRequest;
+import com.nikhil.entities.Flight;
+import com.nikhil.entities.Passenger;
+import com.nikhil.entities.Reservation;
+import com.nikhil.repositories.FlightRepository;
+import com.nikhil.repositories.PassengerRepository;
+import com.nikhil.repositories.ReservationRepository;
+
+@Service
+public class ReservationServiceImpl implements ReservationService {
+	
+	
+	@Autowired
+	private FlightRepository flightRepo;
+	
+	@Autowired
+	private PassengerRepository passengerRepo;
+	
+	@Autowired
+	private ReservationRepository reservationRepo;
+
+	@Override
+	public Reservation bookFlight(ReservationRequest request) {
+		
+		Optional<Flight> findById = flightRepo.findById(request.getId());
+		Flight flight = findById.get();
+		
+		Passenger passenger  = new Passenger();
+		passenger.setFirstName(request.getFirstName());
+		passenger.setLastName(request.getLastName());
+		passenger.setMiddleName(request.getMiddleName());
+		passenger.setEmail(request.getEmail());
+		passenger.setPhone(request.getPhone());
+		
+		passengerRepo.save(passenger);
+		
+		Reservation reservation  = new Reservation();
+		reservation.setFlight(flight);
+		reservation.setPassenger(passenger);
+		reservation.setCheckedIn(false);
+		reservation.setNumberOfBags(0);
+		
+		reservationRepo.save(reservation);
+		
+		return reservation;
+	}
+
+}
 
