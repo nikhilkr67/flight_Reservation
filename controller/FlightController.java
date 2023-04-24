bab8aaf2244c631b2608723e4d81b6b1c42faa5f
Nikhil Kumar <131350052+nikhilkr67@users.noreply.github.com>
2023-04-24 14:41:44 +0530

    Add files via upload

--- controller/FlightController.java
+++ controller/FlightController.java
@@ -1,1 +1,28 @@
+package com.nikhil.controller;
 
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.nikhil.entities.Flight;
+import com.nikhil.repositories.FlightRepository;
+
+@Controller
+public class FlightController {
+	
+	@Autowired
+	private FlightRepository flightRepo;
+	
+	@RequestMapping("/findFlights")
+	public String findFlights(@RequestParam("from") String from,@RequestParam("to") String to, @RequestParam("departureDate") @DateTimeFormat(pattern = "MM-dd-yyyy") Date departureDate, ModelMap modelMap) {
+		List<Flight> flights = flightRepo.findFlights(from, to, departureDate);
+		modelMap.addAttribute("flights", flights);
+		return "displayFlights";
+	}
+}
