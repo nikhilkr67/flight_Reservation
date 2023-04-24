62fb4fcf19dbcb768f7c92c81fe40ebb49553f53
Nikhil Kumar <131350052+nikhilkr67@users.noreply.github.com>
2023-04-24 14:50:45 +0530

    Add files via upload

--- services/ReservationService.java
+++ services/ReservationService.java
@@ -1,1 +1,9 @@
+package com.nikhil.services;
 
+import com.nikhil.dto.ReservationRequest;
+import com.nikhil.entities.Reservation;
+
+public interface ReservationService {
+	Reservation bookFlight(ReservationRequest request);
+
+}
