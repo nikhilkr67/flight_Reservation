c634b5d2abb4278c8e3c9d1d8c115b7307f9e5cc
Nikhil Kumar <131350052+nikhilkr67@users.noreply.github.com>
2023-04-24 14:49:26 +0530

    Add repositories layer

---
+++ repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.nikhil.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nikhil.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	User findByEmail(String email);
+
+}
